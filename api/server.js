262f076495f4e22be4080e4942e4c18486d3403b
deimersonmoreira <deimerson.bhtz@hotmail.com>
2025-10-28 14:50:40 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const pool = require('./db');
+const pool = require('./db'); // db.js na mesma pasta
 
 const app = express();
 app.use(cors());
